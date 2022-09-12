8e2bb8246e1d5fdcee604669a07e082ed4dd237c
yhanini <y.hanini@novactive.com>
2022-09-12 15:35:05 +0100

    test admin

--- app/platformsh-env.php
+++ app/platformsh-env.php
@@ -25,6 +25,7 @@ function mapPlatformShEnvironmentVariables() : void
     // Map services as feasible.
     mapPlatformShPostgreDatabase('postgre', $config);
     mapPlatformShElasticSearch('elasticsearch', $config);
+    mapAdminAppUrl($config);
 }
 
 function mapPlatformShPostgreDatabase(string $relationshipName, Config $config) : void
@@ -56,4 +57,23 @@ function mapPlatformShElasticSearch(string $relationshipName, Config $config) : 
     setEnvVar('ELASTIC_USER', $credentials['username']);
     setEnvVar('ELASTIC_PASS', $credentials['password']);
     setEnvVar('ELASTIC_SCHEME', $credentials['scheme']);
+}
+
+function mapAdminAppUrl(Config $config) : void
+{
+    // If the APP_URL is already set, leave it be.
+    if (getenv('ADMIN_APP_URL')) {
+        return;
+    }
+
+    // If not on Platform.sh, say in a local dev environment, simply
+    // do nothing.  Users need to set the host pattern themselves
+    // in a .env file.
+    if (!$config->inRuntime()) {
+        return;
+    }
+
+    // for test only
+
+    setEnvVar('ADMIN_APP_URL', "https://admin.pr-3-3wpr5ty-pfgzak6qutkji.fr-3.platformsh.site");
 }
