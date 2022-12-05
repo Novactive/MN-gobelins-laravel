263e4d5f6e57ffac84a8df39f20c1ed2507d0a45
Amazighettal <ahettai@techitaway.co>
2022-12-05 12:19:07 +0100

    fix composer error

--- provisioning/scripts/post-create.bash
+++ provisioning/scripts/post-create.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd project
-composer install
+composer update
 curl -XPUT -H "Content-Type: application/json" http://es:9200/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
 php artisan scout:flush "App\Models\Product"
 php artisan scout:import "App\Models\Product"
