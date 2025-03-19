5f61be7c16a11a3d492df73c024709cc2dc26dc2
Ayoub <ayoub.ait-lhaj-lahcen@almaviacx.com>
2025-03-19 12:18:02 +0000

    123800 - RG des objets & optimisation API calls

--- database/migrations/2025_02_25_164306_change_conception_year_to_string_in_products_table.php
+++ database/migrations/2025_02_25_164306_change_conception_year_to_string_in_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNewColumnsToProducts extends Migration
+class ChangeConceptionYearToStringInProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddNewColumnsToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->text('historic')->nullable();
+            $table->string('conception_year')->change();
         });
     }
 
@@ -26,7 +26,7 @@ class AddNewColumnsToProducts extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn(['historic']);
+            $table->integer('conception_year')->change();
         });
     }
 }
