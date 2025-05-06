d11ad24aae4eb03569a86c547541ba963d9113a1
serveur jenkins <serveur.jenkins@localhost>
2025-05-06 11:18:39 +0200

    [25-05-06] release stable-v0.0.1

---
+++ vendor/illuminated/helper-functions/src/debug.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!function_exists('backtrace_as_string')) {
+    /**
+     * Get backtrace without arguments, as a string.
+     *
+     * @return string
+     */
+    function backtrace_as_string()
+    {
+        ob_start();
+
+        debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+
+        return ob_get_clean();
+    }
+}
+
+if (!function_exists('minimized_backtrace_as_string')) {
+    /**
+     * Get minimized backtrace, as a string.
+     *
+     * @return string
+     */
+    function minimized_backtrace_as_string()
+    {
+        $backtrace = explode("\n", backtrace_as_string());
+
+        return collect($backtrace)
+            ->map(function (string $line) {
+                return preg_match('/(#\d+) .*? called at \[(.*?)]/', $line, $matches)
+                    ? "{$matches[1]} {$matches[2]}"
+                    : false;
+            })
+            ->filter()
+            ->implode("\n");
+    }
+}
