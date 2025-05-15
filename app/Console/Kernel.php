9a76a3b0eb1ed986434a00b004331e4721a4a9f1
Lhassan BOUHOU <lhassan.bouhou@almaviacx.com>
2025-05-15 16:53:30 +0100

    feat-127547_Schedule_queue_work - schedule queue every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('gobelins:import:zetcom')->dailyAt('02:00');
+        $schedule->command('artisan queue:work rabbitmq --daemon')->everyMinute()->withoutOverlapping();
     }
 
     /**
