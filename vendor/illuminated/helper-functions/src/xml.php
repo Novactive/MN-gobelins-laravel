f7e9eb5bcd99371a12159023e5609c5038d1b73e
serveur jenkins <serveur.jenkins@localhost>
2025-05-28 17:03:32 +0200

    [25-05-28] release stable-v0.0.6

---
+++ vendor/illuminated/helper-functions/src/xml.php
@@ -0,0 +1,35 @@
+<?php
+
+use Spatie\ArrayToXml\ArrayToXml;
+
+if (!function_exists('xml_to_array')) {
+    /**
+     * Convert the given XML to array.
+     *
+     * @param SimpleXMLElement|string $xml
+     * @return array
+     */
+    function xml_to_array($xml)
+    {
+        if (!($xml instanceof SimpleXMLElement)) {
+            $xml = new SimpleXMLElement($xml);
+        }
+
+        return json_decode(json_encode($xml), true);
+    }
+}
+
+if (!function_exists('array_to_xml')) {
+    /**
+     * Convert the given array to XML string.
+     *
+     * @param array $array
+     * @param string $rootElement
+     * @param bool $replaceSpacesByUnderscoresInKeyNames
+     * @return string
+     */
+    function array_to_xml(array $array, string $rootElement = '', bool $replaceSpacesByUnderscoresInKeyNames = true)
+    {
+        return ArrayToXml::convert($array, $rootElement, $replaceSpacesByUnderscoresInKeyNames);
+    }
+}
