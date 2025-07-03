e43b3d87e2665457b56cf26941fd0efda9e6ed48
serveur jenkins <serveur.jenkins@localhost>
2025-07-03 13:35:48 +0200

    [25-07-03] release stable-v0.7.0

---
+++ vendor/illuminated/helper-functions/src/filesystem.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\Filesystem\Filesystem;
+
+if (!function_exists('relative_path')) {
+    /**
+     * Get a relative path for the given folders.
+     *
+     * @param string $to
+     * @param string $from
+     * @return string
+     */
+    function relative_path(string $to, string $from)
+    {
+        return (new Filesystem)->makePathRelative(realpath($to), realpath($from));
+    }
+}
