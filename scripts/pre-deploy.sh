f37d2cbc1098ae18287e7b25f46c2074702b093e
ahmed.ben-salem.ext.almavia <ahmed.ben-salem@almaviacx.com>
2025-07-24 14:41:41 +0200

    Add command migration - Jenkins pre-deploy script

--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -9,3 +9,10 @@ echo "✅ Script pre-deploy.sh exécuté."
 echo "⚠️  Merci de ne pas supprimer ce fichier, même s’il est vide."
 echo "==================================="
 echo ""
+
+echo "Commande de migration..."
+cd $(readlink  -f /home/collection-pp/www) ; php artisan migrate
+
+echo "🔓 Suppression du mode lecture seule sur tous les index Elasticsearch..."
+curl -XPUT -H "Content-Type: application/json" http://127.0.0.1:9200/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
+
